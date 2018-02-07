578885a89810d80819bff1d55f2f19c7486b5b61
Erk Struwe <e.struwe@neusta.de>
2018-02-07 11:10:42 +0100

    added tsc build

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import * as express from "express";
 
+import { version } from "../package.json";
 import { config } from "./config";
 import { logger } from "./logger";
-import { version } from "../package.json";
 
 const app = express();
 
-app.get('/health', (req, res) => res.send(''));
-app.get('/version', (req, res) => res.send(version));
+app.get("/health", (req, res) => res.send(""));
+app.get("/version", (req, res) => res.send(packageJson.version));
 
 app.listen(config.port, () => {
     logger.info(`listening on port ${config.port}`);
