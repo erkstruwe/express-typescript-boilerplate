c809bad34e3fe71ec14175ca7c235a1139084080
erkstruwe <estruwe@estruwe.de>
2018-05-11 20:32:35 +0200

    vue without compiler

--- src/app/vue.js
+++ src/app/vue.js
@@ -1,7 +1,10 @@
-import Vue from "vue/dist/vue.common"
+import Vue from "vue"
 import VueDemo from "./vue-demo"
 
 const vueDemo = new Vue({
     el: 'vue-demo',
+    render (h) {
+        return h('vue-demo')
+    },
     components: {VueDemo}
 })
